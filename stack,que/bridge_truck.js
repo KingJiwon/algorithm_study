115fb70db25623e709c596d60507040d80acc075
KingJiwon <wldnjs0401@naver.com>
2023-06-26 23:44:43 +0900

    .

--- stack,que/bridge_truck.js
+++ stack,que/bridge_truck.js
@@ -1,40 +1,79 @@
-function solution(bridge_length, weight, truck_weights) {
+function solution(bridge_length, weight, truck_wating) {
   const bridge = new Array(bridge_length).fill(0);
   const truck_crossed = [];
   let time = 0;
-  let bridgeShift = null;
+  let bridgeWeight = 0;
   while (true) {
-    const bridgeWeight = bridge.reduce((acc, cur) => {
+    const bridgeShift = bridge.shift();
+    bridgeWeight = bridge.reduce((acc, cur) => {
       return acc + cur;
     });
-    if (bridgeWeight === 0 && truck_weights.length === 0) {
-      break;
-    }
-    bridgeShift = bridge.shift();
-    if (bridgeShift !== 0) {
-      const shiftedBridgeWeight = bridge.reduce((acc, cur) => {
-        return acc + cur;
-      });
-      truck_crossed.push(bridgeShift);
-      if (shiftedBridgeWeight + truck_weights[0] <= weight) {
-        bridge.push(truck_weights.shift());
-      } else {
-        bridge.push(0);
-      }
-    } else {
-      if (bridgeWeight + truck_weights[0] <= weight) {
-        bridge.push(truck_weights.shift());
-      } else {
-        bridge.push(0);
-      }
-    }
 
+    if (bridgeShift !== 0 && bridgeWeight + truck_wating[0] <= weight) {
+      // 다리에서 빠져나간 트럭이 있고 다음 트럭이 들어올 수 있을 떄 빠져나간 트럭을 완료 트럭 배열에 넣고 대기 트럭을 다리에 올린다.
+      truck_crossed.push(bridgeShift);
+      bridge.push(truck_wating.shift());
+    } else if (
+      bridgeShift !== 0 &&
+      (bridgeWeight + truck_wating[0] > weight || truck_wating.length === 0)
+    ) {
+      // 다리에서 빠져나간 트럭이 있고 다음 트럭이 들어올 수 없거나 기다리는 트럭이 없을 때 빠져나간 트럭을 완료 트럭 배열에 넣고 다리에 빈자리인 0을 넣는다.
+      truck_crossed.push(bridgeShift);
+      bridge.push(0);
+    } else if (bridgeShift === 0 && bridgeWeight + truck_wating[0] <= weight) {
+      // 다리에서 빠져나간 트럭이 없고 다음 트럭이 들어올 수 있을 때 대기 트럭을 shift한 후 다리에 올린다.
+      bridge.push(truck_wating.shift());
+    } else if (
+      bridgeShift === 0 &&
+      (bridgeWeight + truck_wating[0] > weight || truck_wating.length === 0)
+    ) {
+      // 다리에서 빠져나간 트럭이 없고 다음 트럭이 들어올 수 없거나 기다리는 트럭이 없을 때 다리에 빈자리인 0을 넣는다.
+      bridge.push(0);
+    }
     time += 1;
 
-    console.log("truck_crossed :", truck_crossed);
-    console.log("bridge :", bridge);
-    console.log("truck_waitng :", truck_weights);
-    console.log("time :", time);
+    bridgeWeight = bridge.reduce((acc, cur) => {
+      return acc + cur;
+    });
+
+    if (bridgeWeight === 0 && truck_wating.length === 0) {
+      // 이동이 완료 된 후 다리의 무게를 측정했을 때 무게가 0이고 대기트럭이 없으면(트럭이 다 옮겨졌을 때) 종료한다.
+      break;
+    }
   }
   return time;
 }
+
+// function solution(bridge_length, weight, truck_weights) {
+//     const bridge = new Array(bridge_length).fill(0);
+//     const truck_crossed = [];
+//     let time = 0;
+//     while (true) {
+//       const bridgeWeight = bridge.reduce((acc, cur) => {
+//         return acc + cur;
+//       });
+//       if (bridgeWeight === 0 && truck_weights.length === 0) {
+//         break;
+//       }
+//       const bridgeShift = bridge.shift();
+//       if (bridgeShift !== 0) {
+//         const shiftedBridgeWeight = bridge.reduce((acc, cur) => {
+//           return acc + cur;
+//         });
+//         truck_crossed.push(bridgeShift);
+//         if (shiftedBridgeWeight + truck_weights[0] <= weight) {
+//           bridge.push(truck_weights.shift());
+//         } else {
+//           bridge.push(0);
+//         }
+//       } else {
+//         if (bridgeWeight + truck_weights[0] <= weight) {
+//           bridge.push(truck_weights.shift());
+//         } else {
+//           bridge.push(0);
+//         }
+//       }
+//       time += 1;
+//     }
+//     return time;
+//   }
