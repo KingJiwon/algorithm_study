d8f9de73486c825dee341e016c01620cbbefe251
KingJiwon <wldnjs0401@naver.com>
2023-08-24 23:49:38 +0900

    line_up

--- dynamic_programming/line_up.js
+++ dynamic_programming/line_up.js
@@ -1,3 +1,18 @@
+function solution(NumOfChild, childArr) {
+  const dp = new Array(NumOfChild).fill(1); // 각 인덱스 까지 연속적으로 커지는 번호를 가진 아이들의 인원
+  // childArr - 3 7 5 2 6 1 4
+  // dp       - 1 2 2 1 3 1 2
+  for (let i = 1; i < NumOfChild; i++) {
+    let cnt = 0;
+    for (let j = 0; j < i; j++) {
+      if (childArr[j] < childArr[i]) cnt = Math.max(cnt, dp[j]);
+    }
+    dp[i] = cnt + 1;
+  }
+  console.log(dp);
+  console.log(NumOfChild - Math.max(...dp));
+}
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
@@ -19,7 +34,6 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  console.log(count);
-  console.log(childArr);
+  solution(NumOfChild, childArr);
   process.exit();
 });
