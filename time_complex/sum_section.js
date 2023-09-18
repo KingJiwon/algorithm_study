24140cac4f20dd7a2057bc77f473084c66d6d600
KingJiwon <wldnjs0401@naver.com>
2023-09-18 23:38:29 +0900

    sum_section

--- time_complex/sum_section.js
+++ time_complex/sum_section.js
@@ -29,7 +29,7 @@ rl.on("line", (line) => {
 
 rl.on("close", () => {
   numbers.forEach((val, idx) => {
-    sumArr[idx + 1] = sumArr[idx] + val; //숫자들의 누적합 배열 만들기
+    sumArr[idx + 1] = sumArr[idx] + val; //숫자들의 누적합 배열 만들기.
   });
   for (let i = 0; i < info[1]; i++) {
     solution.push(sumArr[sections[i][1]] - sumArr[sections[i][0] - 1]); // 누적합 끼리(end - (start-1)) 빼면 구간별 합이 나옴.
